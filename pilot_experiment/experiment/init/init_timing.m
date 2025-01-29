c6e4222fba922bf62a2df101a2b6f8d11e9b744e
nkphillips <nkphillips@ucsd.edu>
2025-01-29 12:36:03 -0800

    Update init_timing.m

--- pilot_experiment/experiment/init/init_timing.m
+++ pilot_experiment/experiment/init/init_timing.m
@@ -10,24 +10,16 @@ lur003@ucsd.edu
 
 %% Define event durations
 
-t.cue_dur = 2; % s
+t.test_dur = 0.5; % s;
+t.mask_dur = t.test_dur; % s
+t.delay_dur = t.test_dur;
 
-t.init_adaptation_dur = 30; % s; Altan et al. 2024
-t.top_up_adaptation_dur = 0.5; % s; Altan et al. 2024 
+t.response_dur_est = 2; % s
 
-t.test_dur = 0.5; % s; Altan et al. 2024
+t.trial_dur_est = sum([t.test_dur, t.mask_dur, t.delay_dur, t.response_dur_est]); % s
+t.block_dur = 600; % s
 
-t.response_dur = 2; % s
-
-t.rest_dur = t.init_adaptation_dur; % s; this is the minimum rest duration
-t.optional_rest_dur = nan(1, p.num_blocks);
-
-t.trial_dur = t.test_dur + t.response_dur; % s
-
-t.exp_dur_est = ((p.num_blocks * p.num_trials_per_block) * (t.test_dur + t.response_dur) + ...
-     ((p.num_blocks * p.num_trials_per_block) - p.num_blocks) * t.top_up_adaptation_dur + ...
-    (t.init_adaptation_dur * (p.num_blocks-2)) + ...
-    ((p.num_blocks-1) * t.rest_dur)) / 60;
+t.exp_dur_est = [] / 60;
 
 %% Define noise sample update rate
 
@@ -37,6 +29,4 @@ t.noise_sample_dur = 1/t.noise_sample_update_rate; % s
 %% Define frame rate and duration
 
 [t.frame_dur, t.frame_rate] = get_framerate(t); % s, Hz
-
-t.gap_after_adapt = t.frame_dur * 2;
 
