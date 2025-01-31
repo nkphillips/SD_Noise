8689df311f4d02e4c501350bcdc071c92194c9d3
nkphillips <nkphillips@ucsd.edu>
2025-01-31 14:40:06 -0800

    Update init_timing.m

--- pilot_experiment/experiment/init/init_timing.m
+++ pilot_experiment/experiment/init/init_timing.m
@@ -17,9 +17,16 @@ t.delay_dur = t.test_dur;
 t.response_dur_est = 2; % s
 
 t.trial_dur_est = sum([t.test_dur, t.mask_dur, t.delay_dur, t.response_dur_est]); % s
-t.block_dur = 600; % s
 
-t.exp_dur_est = [] / 60;
+t.iti_min = 1;
+t.iti_max = 1.5;
+
+t.iti_dur = t.iti_min + (t.iti_max - t.iti_min) .* rand(p.num_trials, 1);
+
+t.block_dur_est = ((t.trial_dur_est + mean(t.iti_dur(1:p.num_trials_per_block)))  * p.num_trials_per_block) / 60; % s
+t.rest_dur = 30;
+
+t.exp_dur_est = (p.num_trials * t.trial_dur_est + sum(t.iti_dur) + t.rest_dur * (p.num_blocks-1)) / 60;
 
 %% Define noise sample update rate
 
