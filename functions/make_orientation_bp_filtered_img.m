29a5829ba3442334adc717826eb2cf1d9ac63642
luisdramirez <luis.ramirez165@gmail.com>
2025-02-12 16:30:39 -0800

    Create make_orientation_bp_filtered_img.m

---
+++ functions/make_orientation_bp_filtered_img.m
@@ -0,0 +1,43 @@
+
+
+function img = make_orientation_bp_filtered_img(img, orientation, filter_width, ppd)
+
+    %% Create the bandpass filter
+
+    % Define the bandpass filter bounds
+    lower_bound = orientation - filter_width/2;
+    upper_bound = orientation + filter_width/2;
+
+    % Define the bandpass filter
+    bandpass_filter = OrientationBandpass(size(img,1), lower_bound, upper_bound);
+
+    % Smooth the bandpass filter
+    bandpass_filter = imgaussfilt(bandpass_filter, 0.1 * ppd);
+
+    % Normalize the bandpass filter
+    bandpass_filter = normalize_array(bandpass_filter, 'min-max');
+
+    %% FFT the image
+
+    img_fft = fft2(img);
+
+    %% Shift the FFT
+
+    img_fft = fftshift(img_fft);
+
+    %% Apply the bandpass filter
+
+    img_fft = img_fft .* bandpass_filter;
+
+    %% Inverse FFT shift
+
+    img_fft = ifftshift(img_fft);
+
+    %% Inverse FFT to bring back to spatial domain
+
+    img = ifft2(img_fft);
+
+    % Store only the real part of the image
+    img = real(img);
+
+end
