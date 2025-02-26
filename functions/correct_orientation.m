66447bc5f3c849361e33264bf59c9fcba8e02ae3
luisdramirez <luis.ramirez165@gmail.com>
2025-02-26 14:59:01 -0800

    Create correct_orientation.m

---
+++ functions/correct_orientation.m
@@ -0,0 +1,13 @@
+%%% correct_orientation
+
+% take an orientation for a line
+% correct it so that the angle falls along the desired quadrant in "compass axis" (ie vertical = 0°)
+% assume orientation range [0, 180)
+
+function corrected_orientation  = correct_orientation(original_orientation)
+
+    % Transform probe orientation to match the compass axis
+    corrected_orientation = original_orientation + 90;
+    corrected_orientation(corrected_orientation >= 180) = corrected_orientation(corrected_orientation >= 180) - 90;
+
+end
