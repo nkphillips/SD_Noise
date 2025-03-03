94d33fb455610e016ccd6b80ca2a6e116fbba5eb
luisdramirez <luis.ramirez165@gmail.com>
2025-03-03 15:25:12 -0800

    Create calc_probe_orientation.m

---
+++ functions/calc_probe_orientation.m
@@ -0,0 +1,5 @@
+
+function probe_orientation = calc_probe_orientation(test_orientation, probe_offset)
+
+
+    probe_orientation = test_orientation + probe_offset .* datasample([-1 1], length(test_orientation))';
