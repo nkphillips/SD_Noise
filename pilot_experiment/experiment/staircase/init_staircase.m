a118b3aa9f6e3fcf1ebce13539636705529da018
luisdramirez <luis.ramirez165@gmail.com>
2025-03-03 15:25:31 -0800

    Update init_staircase.m

--- pilot_experiment/experiment/staircase/init_staircase.m
+++ pilot_experiment/experiment/staircase/init_staircase.m
@@ -1,5 +1,8 @@
 %%% init_staircase
 
+p.num_conds_names = {'contrast', 'filter'};
+p.num_conds = numel(p.num_conds_names);
+
 % Initialize struct
 staircases = struct();
 staircases.num_staircases_per_cond = 2;
@@ -20,38 +23,54 @@ staircases.num_reversals_to_consider = round(staircases.max_reversals * 0.7);
 
 % Trials
 staircases.max_trials_per_sc = 50; % default = 50
-staircases.num_trials_per_sf = staircases.max_trials_per_sc * staircases.num_staircases_per_cond;
 
 % Arrays
-staircases.probe_offsets = nan(staircases.num_staircases_per_cond , staircases.max_trials_per_sc, p.num_levels);
-staircases.step_size = nan(staircases.num_staircases_per_cond, staircases.max_trials_per_sc, p.num_levels);
-staircases.direction = nan(staircases.num_staircases_per_cond, 2, p.num_levels);
+staircases.probe_offsets = nan(staircases.num_staircases_per_cond , staircases.max_trials_per_sc, p.num_levels, p.num_conds);
+staircases.step_size = nan(staircases.num_staircases_per_cond, staircases.max_trials_per_sc, p.num_levels, p.num_conds);
+staircases.direction = nan(staircases.num_staircases_per_cond, 2, p.num_levels, p.num_conds);
 
-staircases.responses = nan(staircases.num_staircases_per_cond , staircases.max_trials_per_sc, p.num_levels);
-staircases.correct_count = zeros(staircases.num_staircases_per_cond, p.num_levels);
+staircases.test_orientation = nan(staircases.max_trials_per_sc * staircases.num_staircases_per_cond, p.num_levels, p.num_conds);
 
-staircases.trial_order = nan(staircases.max_trials_per_sc * staircases.num_staircases_per_cond, p.num_levels);
-staircases.trial_indices = nan(staircases.num_staircases_per_cond, staircases.max_trials_per_sc, p.num_levels);
+staircases.responses = nan(staircases.num_staircases_per_cond , staircases.max_trials_per_sc, p.num_levels, p.num_conds);
+staircases.correct_count = zeros(staircases.num_staircases_per_cond, p.num_levels, p.num_conds);
 
-staircases.num_reversals = zeros(staircases.num_staircases_per_cond, p.num_levels);
-staircases.reversal_indices = nan(staircases.num_staircases_per_cond, staircases.max_trials_per_sc, p.num_levels);
+staircases.trial_order = nan(staircases.max_trials_per_sc * staircases.num_staircases_per_cond, p.num_levels, p.num_conds);
+staircases.trial_indices = nan(staircases.num_staircases_per_cond, staircases.max_trials_per_sc, p.num_levels, p.num_conds);
+
+staircases.num_reversals = zeros(staircases.num_staircases_per_cond, p.num_levels, p.num_conds);
+staircases.reversal_indices = nan(staircases.num_staircases_per_cond, staircases.max_trials_per_sc, p.num_levels, p.num_conds);
 
 % Initialize staircases
-for n_sf = 1:p.num_levels
+p.num_trials = numel(staircases.trial_order);
+p.num_trials_per_block = staircases.max_trials_per_sc*2;
+p.num_blocks = p.num_conds * p.num_levels;
+for cond = 1:p.num_conds
+    for lvl = 1:p.num_levels
 
-    % Initialize trial order
-    trial_order = repmat(1:staircases.num_staircases_per_cond, 1 , staircases.max_trials_per_sc);
-    staircases.trial_order(:, n_sf) = trial_order(randperm(length(trial_order)));
+        % Initialize trial order
+        trial_order = repmat(1:staircases.num_staircases_per_cond, 1 , staircases.max_trials_per_sc);
+        staircases.trial_order(:, lvl, cond) = trial_order(randperm(length(trial_order)));
 
-    % Initialize direction
-    staircases.direction(:, :, n_sf) = [1 1; -1 -1];
+        % Initialize direction
+        staircases.direction(:, :, lvl, cond) = [1 1; -1 -1];
 
-    for n_sc = 1:staircases.num_staircases_per_cond
+        for n_sc = 1:staircases.num_staircases_per_cond
 
-        staircases.trial_indices(n_sc, :, n_sf) = find(staircases.trial_order(:, n_sf) == n_sc);
-        staircases.probe_offsets(n_sc, 1, n_sf) = staircases.init_probe_offset(n_sc);
-        staircases.step_size(n_sc, 1, n_sf) = staircases.init_step_size(n_sc);
+            staircases.trial_indices(n_sc, :, lvl, cond) = find(staircases.trial_order(:, lvl, cond) == n_sc);
+            staircases.probe_offsets(n_sc, 1, lvl, cond) = staircases.init_probe_offset(n_sc);
+            staircases.step_size(n_sc, 1, lvl, cond) = staircases.init_step_size(n_sc);
+
+        end
+
+        % Generate test orientations
+        % use the datasample() function to generate a sequence of test orientations from a min and max range of orientation values,
+        % equal in size to the staircase structure (eg., staircases.probe_offsets)
+        data = stimuli.orientation_min:stimuli.orientation_max;
+        staircases.test_orientation() = datasample(data, p.num_trials_per_block); 
 
     end
-
 end
+
+
+%
+
