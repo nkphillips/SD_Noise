8cb775eea63f1bef9a21653d96bb25a3ad3f68aa
luisdramirez <luis.ramirez165@gmail.com>
2025-03-03 15:25:25 -0800

    Update discrimination_staircase.m

--- pilot_experiment/experiment/staircase/discrimination_staircase.m
+++ pilot_experiment/experiment/staircase/discrimination_staircase.m
@@ -1,181 +1,120 @@
 %%% detection_staircase
 % 2 staircases per inducer SF that begin at the min and max contrast value
 
-for curr_sf = 1:length(p.inducer_sfs)
-   
-    if p.demo_run
-        disp(['SF = ' num2str(p.inducer_sfs(curr_sf)) ' cpd']);
-    end
+for curr_cond = 1:p.num_conds
+    for curr_lvl = 1:p.num_levels
 
-    for n_trial = 1:length(staircases.trial_order(:, curr_sf))
-      
-        %% Get current staircase info
+        for n_trial = 1:length(staircases.trial_order(:, curr_lvl, curr_cond))
 
-        curr_sc = staircases.trial_order(n_trial, curr_sf);
-        
-        % Skip current trial if the max reversals have already been reached for the current staircase
-        if staircases.num_reversals(curr_sc, curr_sf) == staircases.max_reversals
-            % continue
-        end
-        
-        % Get the current staircase trial number
-        curr_sc_trial = find(n_trial == staircases.trial_indices(curr_sc, :, curr_sf));
+            % Get current test orientation
+            curr_test_orient = p.trial_events(n_trial, 1, n_block);
 
-        if p.demo_run   
-            disp(['Staircase ' num2str(curr_sc) ', trial ' num2str(curr_sc_trial)])
-        end
 
-        %% Make contrast-adjusted textures
-
-        % Get current contrast
-        curr_contrast = min(1, stimuli.base_contrast + staircases.contrast_deltas(curr_sc, curr_sc_trial, curr_sf));
-
-        % Display current contrast
-        if p.demo_run
-            disp(['Contrast delta = ' num2str(100*(curr_contrast - stimuli.base_contrast)) '%'])
-        end
-
-        % Store current contrast
-        staircases.contrast(curr_sc, curr_sc_trial, curr_sf) = curr_contrast;
-
-        % Make contrast-adjusted textures
-        contrast_textures = squeeze(stimuli.full_contrast_textures(:,:, curr_sf, :)) .* curr_contrast;
-        contrast_textures_made = nan(1, p.num_samples);
-        for i = 1:p.num_samples
-            contrast_textures_made(i) = Screen('MakeTexture', w.window, contrast_textures(:,:,i));
-        end
-
-        %% Draw textures
-
-        n_noise_sample = 0;
-        
-        for n_frame = 1:frames.frames_count
-
-            % Update noise sample
-            if frames.noise_sample_update(n_frame)
-                n_noise_sample = n_noise_sample + 1;
-                curr_noise_sample = frames.update_noise_sample_seq(n_trial, n_noise_sample, curr_sf);
-            end
-
-            % Draw stimulus with correct contrast 
-            if frames.contrast_delta(n_trial, n_frame, curr_sf) == 1
-                Screen('DrawTexture', w.window, contrast_textures_made(curr_noise_sample))
-            else
-                Screen('DrawTexture', w.window, stimuli.textures_made(curr_sf, curr_noise_sample))
-            end
-
-            % Draw Aperture
-            Screen('DrawTexture', w.window, stimuli.aperture_made, [], aperture_patch)
-
-            % Draw Fixation
-            Screen('DrawTexture', w.window, fixation_space_made, [], fixation_space_patch); % Fixation circle
-            Screen('FillOval', w.window, p.fixation_dot_color, fixation_dot_patch); % Fixation dot
-
-            % Set frame onsets if the first frame
-            if n_frame == 1
-                frame_onsets = frames.frame_onsets + GetSecs;
-            end
-
-            % Flip stimuli
-            Screen('Flip', w.window, frame_onsets(n_frame));
-
-        end
-
-        %% Response
-
-        % Initialize response state and response start
-        no_response = 1;
-        response_start = GetSecs;
-
-        % Check for a key press
-        if ~p.simulate_response
-
-            [key_pressed, first_press] = KbQueueCheck(p.device_number);
-            which_press = find(first_press);
-            response_dur = GetSecs - response_start;
-
-        else
-           
-            key_pressed = datasample([0 1], 1, 'Weights',[1-presumed_target presumed_target]);
-            if key_pressed
-                which_press = p.keypress_numbers(1); 
-                response_dur = GetSecs - response_start;
-                if p.demo_run
-                    disp('Change detected')
+            if p.demo_run
+                disp(['Trial ' num2str(n_trial)])
+                disp(['Test Orientation: ' num2str(curr_test_orient) '°'])
+                disp(['Test Contrast: ' num2str(round(100*stimuli.contrast(curr_contrast),2)) '%'])
+                disp(['Test Filter Width: ' num2str(stimuli.bp_filter_width(curr_filter_width)) '°'])
+                if curr_probe_orient > 90
+                    disp(['Old Probe Orientation: ' num2str(curr_probe_orient-90) '°'])
+                else
+                    disp(['Old Probe Orientation: ' num2str(curr_probe_orient+270) '°'])
                 end
-            else
-                staircases.responses(curr_sc, curr_sc_trial, curr_sf) = 0;
-                if p.demo_run
-                    disp('Change missed')
-                end
+                disp(['Corrected Probe Orientation: ' num2str(curr_probe_orient) '°'])
             end
 
-        end
+            %% Get current staircase info
 
-        % Evaluate response
-        if key_pressed && no_response
+            curr_sc = staircases.trial_order(n_trial, curr_lvl, curr_cond);
 
-            % Find overlap between pressed keys and relevant keys
-            relevant_keys = intersect(which_press, [0 p.keypress_numbers(1)]);
-            if ~isempty(relevant_keys) % If a relevant key was pressed
+            % Skip current trial if the max reversals have already been reached for the current staircase
+            if staircases.num_reversals(curr_sc, curr_lvl, curr_cond) == staircases.max_reversals
+                % continue
+            end
 
-                response_dur(n_trial) = response_dur; % Record response duration
-                first_relevant_key = relevant_keys(1); % Get the first relevant key
+            % Get the current staircase trial number
+            curr_sc_trial = find(n_trial == staircases.trial_indices(curr_sc, :, curr_lvl, curr_cond));
 
-                % Store response based on the pressed key
-                if first_relevant_key == p.keypress_numbers(1)
-                    staircases.responses(curr_sc, curr_sc_trial, curr_sf) = 1;
+            if p.demo_run
+                disp(['Staircase ' num2str(curr_sc) ', trial ' num2str(curr_sc_trial)])
+            end
+
+
+            % Probe offset
+
+
+
+            % Create rotated probe line
+            curr_probe_orient = p.trial_events(n_trial, 2, n_block); % Get current probe orientation
+            curr_probe_orient_rad = deg2rad(curr_probe_orient);
+            curr_rotation = [cos(curr_probe_orient_rad), -sin(curr_probe_orient_rad); ...
+                sin(curr_probe_orient_rad), cos(curr_probe_orient_rad)];
+
+            curr_probe_line = curr_rotation * (stimuli.probe_line_base - [w.centerX; w.centerY]);
+            curr_probe_line = curr_probe_line + [w.centerX; w.centerY];
+
+            
+            %% Draw Test
+
+
+            %% Draw mask
+
+
+            %% Delay period
+
+            for n_frame = 1:frames.delay_frames_count
+
+                % Draw fixation
+                Screen('DrawTexture', w.window, fixation_space_made, [], fixation_space_patch); % Fixation circle
+                Screen('FillOval', w.window, p.fixation_dot_color, fixation_dot_patch); % Fixation dot
+
+                % Flip
+                if n_frame == 1
+                    delay_frames_onsets = frames.delay_frames_onsets + GetSecs;
                 end
-
-                % No response is now false
-                no_response = 0;
-
-            elseif any(which_press == p.keypress_numbers(end)) % Escape key
-
-                if p.demo_run
-                    disp('Escape key pressed. Exiting...');
-                end
-                return; % Exit the experiment
+                Screen('Flip', w.window, delay_frames_onsets(n_frame)); % every frame has a deadline
 
             end
 
-        end
+            %% Draw probe
 
-        %% ITI
 
-        iti_start = GetSecs;
+            %% Response
 
-        while GetSecs - iti_start <= t.iti_dur
-        
-            % Draw Fixation
-            Screen('FillOval', w.window, p.fixation_dot_color, fixation_dot_patch); % Fixation dot
-
-            % Flip stimuli
-            Screen('Flip', w.window);
+            % Initialize response state and response start
+            no_response = 1;
+            response_start = GetSecs;
 
             % Check for a key press
             if ~p.simulate_response
+
                 [key_pressed, first_press] = KbQueueCheck(p.device_number);
                 which_press = find(first_press);
                 response_dur = GetSecs - response_start;
+
             else
-            
+
                 key_pressed = datasample([0 1], 1, 'Weights',[1-presumed_target presumed_target]);
                 if key_pressed
-                    which_press = p.keypress_numbers(1); 
+                    which_press = p.keypress_numbers(1);
                     response_dur = GetSecs - response_start;
+                    if p.demo_run
+                        disp('Change detected')
+                    end
                 else
-                    staircases.responses(curr_sc, curr_sc_trial, curr_sf) = 0;
+                    staircases.responses(curr_sc, curr_sc_trial, curr_lvl) = 0;
+                    if p.demo_run
+                        disp('Change missed')
+                    end
                 end
 
-            end 
-       
+            end
+
             % Evaluate response
             if key_pressed && no_response
 
                 % Find overlap between pressed keys and relevant keys
                 relevant_keys = intersect(which_press, [0 p.keypress_numbers(1)]);
-
                 if ~isempty(relevant_keys) % If a relevant key was pressed
 
                     response_dur(n_trial) = response_dur; % Record response duration
@@ -183,10 +122,7 @@ for curr_sf = 1:length(p.inducer_sfs)
 
                     % Store response based on the pressed key
                     if first_relevant_key == p.keypress_numbers(1)
-                        staircases.responses(curr_sc, curr_sc_trial, curr_sf) = 1;
-                        if p.demo_run
-                            disp('Change detected')
-                        end
+                        staircases.responses(curr_sc, curr_sc_trial, curr_lvl) = 1;
                     end
 
                     % No response is now false
@@ -201,26 +137,89 @@ for curr_sf = 1:length(p.inducer_sfs)
 
                 end
 
-            end 
+            end
 
-        end
+            %% ITI
 
-        if p.demo_run
+            if n_trial < length(staircases.trial_order(:, curr_lvl, curr_cond))
+                iti_start = GetSecs;
 
-            if no_response
-                disp('Change missed')
+                while GetSecs - iti_start <= t.iti_dur
+
+                    % Draw Fixation
+                    Screen('FillOval', w.window, p.fixation_dot_color, fixation_dot_patch); % Fixation dot
+
+                    % Flip stimuli
+                    Screen('Flip', w.window);
+
+                    % Check for a key press
+                    if ~p.simulate_response
+                        [key_pressed, first_press] = KbQueueCheck(p.device_number);
+                        which_press = find(first_press);
+                        response_dur = GetSecs - response_start;
+                    else
+
+                        key_pressed = datasample([0 1], 1, 'Weights',[1-presumed_target presumed_target]);
+                        if key_pressed
+                            which_press = p.keypress_numbers(1);
+                            response_dur = GetSecs - response_start;
+                        else
+                            staircases.responses(curr_sc, curr_sc_trial, curr_lvl) = 0;
+                        end
+
+                    end
+
+                    % Evaluate response
+                    if key_pressed && no_response
+
+                        % Find overlap between pressed keys and relevant keys
+                        relevant_keys = intersect(which_press, [0 p.keypress_numbers(1)]);
+
+                        if ~isempty(relevant_keys) % If a relevant key was pressed
+
+                            response_dur(n_trial) = response_dur; % Record response duration
+                            first_relevant_key = relevant_keys(1); % Get the first relevant key
+
+                            % Store response based on the pressed key
+                            if first_relevant_key == p.keypress_numbers(1)
+                                staircases.responses(curr_sc, curr_sc_trial, curr_lvl) = 1;
+                                if p.demo_run
+                                    disp('Change detected')
+                                end
+                            end
+
+                            % No response is now false
+                            no_response = 0;
+
+                        elseif any(which_press == p.keypress_numbers(end)) % Escape key
+
+                            if p.demo_run
+                                disp('Escape key pressed. Exiting...');
+                            end
+                            return; % Exit the experiment
+
+                        end
+
+                    end
+
+                end
+            end
+
+            %% Update staircase
+
+            update_staircase
+
+            if p.demo_run
+                disp(' ')
+                % KbWait;
             end
 
         end
-        
-        %% Update staircase
 
-        update_staircase
 
-        if p.demo_run
-            disp(' ')
-            % KbWait;
-        end
+        %% Rest period
+
         
+
     end
 end
