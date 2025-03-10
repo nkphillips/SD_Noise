defd04b103e96652a37dc236423b498367ec057f
luisdramirez <luis.ramirez165@gmail.com>
2025-03-10 15:40:21 -0700

    Update update_staircase.m

--- pilot_experiment/experiment/staircase/update_staircase.m
+++ pilot_experiment/experiment/staircase/update_staircase.m
@@ -2,6 +2,14 @@
 
 %% 2-down 1-up rule
 
+%%
+
+
+curr_probe_offset = staircases.probe_offsets(curr_sc, curr_sc_trial, curr_lvl, curr_cond);
+curr_step_size = staircases.step_size(curr_sc, curr_sc_trial, curr_lvl, curr_cond);
+
+%%
+
 % The current direction is stored as the previous direction
 staircases.direction(curr_sc, 1, curr_lvl, curr_cond) = staircases.direction(curr_sc, 2, curr_lvl, curr_cond);
 
@@ -14,29 +22,27 @@ if staircases.responses(curr_sc, curr_sc_trial, curr_lvl, curr_cond) == 1
         disp(['Correct responses: ' num2str(staircases.correct_count(curr_sc, curr_lvl, curr_cond))])
     end
 
-    % Update contrast if there have been 2 correct responses
+    % Update direction
+    staircases.direction(curr_sc, 2, curr_lvl, curr_cond) = -1;
+
+    % Update probe offset if there have been 2 correct responses
     if staircases.correct_count(curr_sc, curr_lvl, curr_cond) == 2
         
-        % Update direction
-        staircases.direction(curr_sc, 2, curr_lvl, curr_cond) = -1; % the current direction is updated as decreasing
-
         % Decrease probe offset for next trial of the current staircase
         if curr_sc_trial < staircases.max_trials_per_sc
-            staircases.probe_offsets(curr_sc, curr_sc_trial+1, curr_lvl, curr_cond) = max(staircases.min_probe_offset, staircases.probe_offsets(curr_sc, curr_sc_trial, curr_lvl, curr_cond) - staircases.step_size(curr_sc, curr_sc_trial, curr_lvl, curr_cond));
+            staircases.probe_offsets(curr_sc, curr_sc_trial+1, curr_lvl, curr_cond) = max(staircases.min_probe_offset, curr_probe_offset - curr_step_size);
         end
 
         % Reset counter
         staircases.correct_count(curr_sc, curr_lvl, curr_cond) = 0;
     
     elseif staircases.correct_count(curr_sc, curr_lvl, curr_cond) == 1
-
-        % Update direction
-        staircases.direction(curr_sc, 2, curr_lvl, curr_cond) = -1;
         
         % Next probe offset is the same as the current probe offset
         if curr_sc_trial < staircases.max_trials_per_sc
-            staircases.probe_offsets(curr_sc, curr_sc_trial+1, curr_lvl, curr_cond) = staircases.probe_offsets(curr_sc, curr_sc_trial, curr_lvl, curr_cond);
+            staircases.probe_offsets(curr_sc, curr_sc_trial+1, curr_lvl, curr_cond) = curr_probe_offset;
         end
+
     end
 
 else
@@ -68,8 +74,11 @@ end
 
 %% Check for reversal
 
-% If the direction of the current trial is different from the previous trial
-if staircases.direction(curr_sc, 1, curr_lvl, curr_cond) ~= staircases.direction(curr_sc, 2, curr_lvl, curr_cond) && curr_sc_trial > 2
+% If the direction of the previous trial is different from the current trial
+prev_direction = staircases.direction(curr_sc, 1, curr_lvl, curr_cond);
+curr_direction = staircases.direction(curr_sc, 2, curr_lvl, curr_cond);
+
+if prev_direction ~= curr_direction 
 
     % Increment reversal counter
     staircases.num_reversals(curr_sc, curr_lvl, curr_cond) = staircases.num_reversals(curr_sc, curr_lvl, curr_cond) + 1;
@@ -79,30 +88,29 @@ if staircases.direction(curr_sc, 1, curr_lvl, curr_cond) ~= staircases.direction
 
     disp('Reversal detected.')
 
-end
+    %% Update step size
 
-%% Update step size
+    % Reduce step size after some reversals
+    if staircases.num_reversals(curr_sc, curr_lvl, curr_cond) >= staircases.max_reversals/2 && curr_sc_trial < staircases.max_trials_per_sc
 
-% Reduce step size after some reversals
-if staircases.num_reversals(curr_sc, curr_lvl, curr_cond) >= staircases.max_reversals/2 && curr_sc_trial < staircases.max_trials_per_sc
-    
-    % Get the current step size
-    old_step_size = staircases.step_size(curr_sc, curr_sc_trial, curr_lvl, curr_cond);
+        % Get the current step size
+        old_step_size = staircases.step_size(curr_sc, curr_sc_trial, curr_lvl, curr_cond);
 
-    % Reduce step size
-    staircases.step_size(curr_sc, curr_sc_trial+1, curr_lvl, curr_cond) = max(staircases.min_step_size, staircases.step_size(curr_sc, curr_sc_trial, curr_lvl, curr_cond) / 2);
+        % Reduce step size
+        staircases.step_size(curr_sc, curr_sc_trial+1, curr_lvl, curr_cond) = max(staircases.min_step_size, staircases.step_size(curr_sc, curr_sc_trial, curr_lvl, curr_cond) / 2);
 
-    % Ensure step size does not exceed max step size
-    staircases.step_size(curr_sc, curr_sc_trial+1, curr_lvl, curr_cond) = min(staircases.max_step_size, staircases.step_size(curr_sc, curr_sc_trial+1, curr_lvl, curr_cond));
+        % Ensure step size does not exceed max step size
+        staircases.step_size(curr_sc, curr_sc_trial+1, curr_lvl, curr_cond) = min(staircases.max_step_size, staircases.step_size(curr_sc, curr_sc_trial+1, curr_lvl, curr_cond));
 
-    if p.demo_run
-        disp(['Staircase step size reduced from ' num2str(100*old_step_size) ' to ' num2str(100*staircases.step_size(curr_sc, curr_sc_trial+1, curr_lvl, curr_cond)) '%'])
+        if p.demo_run
+            disp(['Staircase step size reduced from ' num2str(100*old_step_size) ' to ' num2str(100*staircases.step_size(curr_sc, curr_sc_trial+1, curr_lvl, curr_cond)) '%'])
+        end
+
+    else
+
+        % Next trial step size is the same as the current trial step size
+        staircases.step_size(curr_sc, curr_sc_trial+1, curr_lvl, curr_cond) = staircases.step_size(curr_sc, curr_sc_trial, curr_lvl, curr_cond);
+
     end
 
-else
-
-    % Next trial step size is the same as the current trial step size
-    staircases.step_size(curr_sc, curr_sc_trial+1, curr_lvl, curr_cond) = staircases.step_size(curr_sc, curr_sc_trial, curr_lvl, curr_cond);
-
 end
-
