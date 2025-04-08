3166f1bf0c0f4fe3b80bec15381c5b62d38ff92b
luisdramirez <luis.ramirez165@gmail.com>
2025-04-08 15:45:07 -0700

    Update correct_orientation.m

--- functions/correct_orientation.m
+++ functions/correct_orientation.m
@@ -1,13 +1,11 @@
 %%% correct_orientation
 
-% take an orientation for a line
-% correct it so that the angle falls along the desired quadrant in "compass axis" (ie vertical = 0°)
-% assume orientation range [0, 180)
+% Take an orientation for a line to use in Screen('DrawLine')
+% Correct it so that the angle falls along the desired quadrant in "compass axis" (ie vertical = 0°)
+% Assume orientation range [0, 180)
 
 function corrected_orientation = correct_orientation(original_orientation)
 
-    % Transform probe orientation to match the compass axis
     corrected_orientation = original_orientation + 90;
-    corrected_orientation(corrected_orientation >= 180) = corrected_orientation(corrected_orientation >= 180) - 90;
 
 end
