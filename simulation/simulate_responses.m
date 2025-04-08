caa1c60a7604a8d8dc5eb57bb10bf8198ecf0f26
nkphillips <nkphillips@ucsd.edu>
2025-04-08 12:48:35 -0700

    Update simulate_responses.m

--- simulation/simulate_responses.m
+++ simulation/simulate_responses.m
@@ -1,4 +1,4 @@
-function responses = simulate_responses(p)
+function [responses, correct] = simulate_responses(p)
 
     % Calculate orientation difference
     orient_diff = p.trial_events(:, 2) - p.trial_events(:, 1);
@@ -23,20 +23,23 @@ function responses = simulate_responses(p)
     mu = p.psychometric_params(1); % ie threshold
     sigma = p.psychometric_params(2); % ie slope
     guess_rate = p.psychometric_params(3);
-    p_CW = (1 - guess_rate) * normcdf(internal_orient_diff, mu, sigma) + 0.5 * guess_rate;
+
+    p_CW = calc_pCW(internal_orient_diff, mu, sigma, guess_rate);
     
-    % Simulate response based on discrimination probability
-    responses = rand() < p_CW;
+    % Simulate responses (1 = CW)
+    responses = rand(length(p_CW), 1) < p_CW;
+    
+    % Determine correct vs incorrect responses based on sign of offset
+    is_CW = orient_diff > 0;
+    is_CCW = orient_diff < 0;
+
+    correct = (responses == 1 & is_CW) | (responses == 0 & is_CCW);
 
 end
 
 
 
-% Determine correct vs incorrect responses based on sign of offset
-%     is_CW = orient_diff > 0;
-%     is_CCW = orient_diff < 0;
-% 
-%     correct = (responses == 1 & is_CW) | (responses == 0 & is_CCW);
+
 % 
 % % Store all relevant trial-level data in a debug matrix
 %     delta_theta_padded = [delta_theta; NaN];
@@ -52,46 +55,19 @@ end
 
 
 
-% Probability CW
+% % Evaluate correctness
+% 
+% function correct = correct_response(responses, probe_offsets)
+%     % Determine correctness based on probe offset direction
+%     correct = (responses == 1 & probe_offsets > 0) | ...
+%               (responses == 0 & probe_offsets < 0);
+% end
+% 
+% 
+% 
+% 
+% 
+% %% Gridsearch
+% 
 
-function p_CW = calc_pCW(internal_diff, psychometric_params)
-    % Calculate the probability of CW response from internal orientation difference
-    mu = psychometric_params(1);
-    sigma = psychometric_params(2);
-    lapse = psychometric_params(3);
-
-    p_CW = (1 - lapse) * normcdf(internal_diff, mu, sigma) + 0.5 * lapse;
-end
-
-% Evaluate correctness
-
-function correct = correct_response(responses, probe_offsets)
-    % Determine correctness based on probe offset direction
-    correct = (responses == 1 & probe_offsets > 0) | ...
-              (responses == 0 & probe_offsets < 0);
-end
-
-
-
-
-
-%% Gridsearch
-
-function [best_params, best_sse] = grid_search_mu_sigma(offsets, pCW)
-
-    mu_vals = -10:0.5:10;
-    sigma_vals = 0.5:0.5:10;
-    best_sse = inf;
-
-    for n = mu_vals
-        for i = sigma_vals
-            predicted_values = normcdf(offsets, n, i);
-            sse = sum((pCW - predicted_values).^2);
-            if sse < best_sse
-                best_sse = sse;
-                best_params = [n, i];
-            end
-        end
-    end
-end
 
