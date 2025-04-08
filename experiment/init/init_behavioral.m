b6e7a521f284af9a0e8fbaeeea4958cc9690dbd6
luisdramirez <luis.ramirez165@gmail.com>
2025-04-08 15:44:35 -0700

    Update init_behavioral.m

--- experiment/init/init_behavioral.m
+++ experiment/init/init_behavioral.m
@@ -2,5 +2,7 @@
 
 %% Pre-allocate fields
 
-behav_data.response = nan(p.num_trials,p.num_blocks);
+behav_data.response = nan(p.num_trials_per_block,p.num_blocks);
+behav_data.correct = nan(p.num_trials_per_block, p.num_blocks);
 
+behav_data.performance = nan(1, p.num_blocks);
