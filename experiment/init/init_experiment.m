962302c465f5deb4a68143d596f6d93a1279123b
luisdramirez <luis.ramirez165@gmail.com>
2025-04-15 10:48:53 -0700

    Update init_experiment.m

--- experiment/init/init_experiment.m
+++ experiment/init/init_experiment.m
@@ -45,9 +45,9 @@ end
 
 if p.training 
     % Note that the number of levels for each condition in training is 1
-    p.num_trials_per_cond = 1;  
+    p.num_trials_per_cond = 5; % Default = 40
 else
-    p.num_trials_per_cond = 1; % default = 40
+    p.num_trials_per_cond = 40; % Default = 40
 end
 
 %% Generate level order, orientations, correct response
