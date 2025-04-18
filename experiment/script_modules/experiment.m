94305f4a90057a35e6d0aebd57468327b749dc87
luisdramirez <luis.ramirez165@gmail.com>
2025-04-18 15:42:05 -0700

    Update experiment.m

--- experiment/script_modules/experiment.m
+++ experiment/script_modules/experiment.m
@@ -16,7 +16,6 @@ Created 10.24.2024
 if ~p.simulate_response, wait_for_trigger; end
 t.exp_start_time = GetSecs;
 
-
 %% Block loop
 
 for n_block = 1:p.num_blocks
@@ -26,37 +25,45 @@ for n_block = 1:p.num_blocks
     % Pull the current block condition
     curr_cond = p.block_order(n_block);
 
-    % Update probe offsets if it's not the first block of the current condition
-    if n_block > first_block(curr_cond)
-    
-        prev_block = find(p.block_order(1:n_block-1) == curr_cond, 1, 'last');
-        prev_correct = behav_data.correct(:, prev_block);
-        prev_lvl_order = p.trial_events(:, level_order_col, prev_block);
+    if p.use_staircase
 
-        probe_offsets = p.probe_offsets(curr_cond,:);
-        step_size = squeeze(mean(p.staircases.step_size(:,end,:,curr_cond),1));
-
-        new_probe_offsets = update_probe_offset(prev_correct, prev_lvl_order, probe_offsets, step_size, p.staircases.min_probe_offset, p.staircases.max_probe_offset);
+        % Update probe offsets if it's not the first block of the current condition
+        if n_block > first_block(curr_cond)
         
-        if p.disp_on
-            disp('Old probe offsets:');
-            disp(num2str(probe_offsets));
-            disp('New probe offsets:');
-            disp(num2str(new_probe_offsets)); 
+            prev_block = find(p.block_order(1:n_block-1) == curr_cond, 1, 'last');
+            prev_correct = behav_data.correct(:, prev_block);
+            prev_lvl_order = p.trial_events(:, level_order_col, prev_block);
+
+            probe_offsets = p.probe_offsets(curr_cond,:);
+            step_size = squeeze(mean(p.staircases.step_size(:,end,:,curr_cond),1));
+
+            new_probe_offsets = update_probe_offset(prev_correct, prev_lvl_order, probe_offsets, step_size, p.staircases.min_probe_offset, p.staircases.max_probe_offset);
+            
+            if p.disp_on
+                disp('Old probe offsets:');
+                disp(num2str(probe_offsets));
+                disp('New probe offsets:');
+                disp(num2str(new_probe_offsets)); 
+            end
+
+            p.probe_offsets(curr_cond,:) = new_probe_offsets;
+        
+            curr_probe_offsets = p.probe_offsets(curr_cond, p.trial_events(:, level_order_col, n_block))';
+            probe_orientation = calc_probe_orientation(p.trial_events(:,test_orientation_col, n_block), curr_probe_offsets);
+            p.trial_events(:, probe_orientation_col, n_block) = probe_orientation;
+            
         end
 
-        p.probe_offsets(curr_cond,:) = new_probe_offsets;
+    else
 
+        probe_orientation = p.trial_events(:, probe_orientation_col, n_block);
+    
     end
 
-    % Calculate probe orientations (test_orientation ± probe_offset)
-    curr_probe_offsets = p.probe_offsets(curr_cond, p.trial_events(:, level_order_col, n_block))';
-    test_orientation = p.trial_events(:,test_orientation_col, n_block);
-    probe_orientation = calc_probe_orientation(test_orientation, curr_probe_offsets);
-    p.trial_events(:,probe_orientation_col,n_block) = correct_orientation(probe_orientation)'; % Transform probe orientation to match the compass axis
-
+    corrected_probe_orientations = correct_orientation(probe_orientation)'; % Transform probe orientation to match the compass axis
+    
     % Storing correct response
-    cclockwise_trials = double(probe_orientation < test_orientation);
+    cclockwise_trials = double(probe_orientation < p.trial_events(:,test_orientation_col, n_block));
     cclockwise_trials(cclockwise_trials == 0) = 2;
     p.correct_response(:,n_block) = cclockwise_trials;
 
