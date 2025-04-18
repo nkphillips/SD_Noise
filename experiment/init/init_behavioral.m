711c24a74fd9c7493748a2c356755d2d812fd247
luisdramirez <luis.ramirez165@gmail.com>
2025-04-17 21:51:25 -0700

    Update init_behavioral.m

--- experiment/init/init_behavioral.m
+++ experiment/init/init_behavioral.m
@@ -5,4 +5,4 @@
 behav_data.response = nan(p.num_trials_per_block,p.num_blocks);
 behav_data.correct = nan(p.num_trials_per_block, p.num_blocks);
 
-behav_data.performance = nan(1, p.num_blocks);
+behav_data.performance = nan(p.num_levels, p.num_conds);
