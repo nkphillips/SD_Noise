78abd64d6f92be316ad668d9fdf1b985ff3adb08
luisdramirez <luis.ramirez165@gmail.com>
2025-04-19 15:44:56 -0700

    Update open_window.m

--- experiment/script_modules/open_window.m
+++ experiment/script_modules/open_window.m
@@ -8,17 +8,6 @@
 %% Open Window
 
 w.window = PsychImaging('OpenWindow', w.use_screen, w.bg_color, [0 0 w.screen_width_px w.screen_height_px]);
-[w.screen_width_px, w.screen_height_px] = Screen('WindowSize', w.window);
-
-w.screen_width_mm = Screen('DisplaySize', w.use_screen);
-w.screen_width_cm = w.screen_width_mm / 10;
-
-w.visual_angle = 2 * atan2d(w.screen_width_cm/2,  w.view_distance);
-w.px_per_cm = w.screen_width_px / w.screen_width_cm;
-w.px_size = 1 / (w.screen_width_px / w.screen_width_cm);
-w.cm_per_deg = 2 * w.view_distance * tand(0.5);
-w.ppd = w.px_per_cm * w.cm_per_deg;
-w.f_Nyquist = 1/(2*w.px_size);
 
 if ~p.half_screen
     HideCursor;
