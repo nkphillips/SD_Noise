8400d40478546476030b64c339e3f27cff85155c
luisdramirez <luis.ramirez165@gmail.com>
2025-05-05 16:27:01 -0700

    Update SD_Noise_Analyses_And_Figures.m

--- analyses/SD_Noise_Analyses_And_Figures.m
+++ analyses/SD_Noise_Analyses_And_Figures.m
@@ -4,27 +4,46 @@
 
 clc; close all; clear all;
 
-which_setup = 'simulated';
-analysis_date = '11.18.2024';
-
-init_paths
-
-subj_IDs = {'900' '901' '902' '903' '904' '905' '906' '907' '908' '909'};  
-cond_names = {'contrast' 'filter'};
-
-num.subjs = length(subj_IDs);
-num.conds = length(cond_names);
-
 %% Toggles
 
 plot_ind = 1;
 plot_grp = 1;
 
 save_ind_figures = 1;
-save_grp_figures = 1;
+save_grp_figures = 0;
 
 estimate_response_bias = 1;
 estimate_sd_bias = 1;
+
+toggles.disp_on = 1;
+
+%% Hard coded variables
+
+which_setup = '3329C_ASUS';
+analysis_date = '04.29.2025';
+
+subj_IDs = {'002'};  
+cond_names = {'contrast' 'filter'};
+
+num.subjs = length(subj_IDs);
+num.conds = length(cond_names);
+num.blocks = 6;
+num.levels = 3;
+num.blocks_per_cond = num.blocks/num.conds;
+num.cond_combos = num.conds * num.levels;
+
+% Model fitting options
+options = optimoptions('fmincon');
+lower_bounds = [-15, 1]; % lower bounds for [mu, sigma]
+upper_bounds = [15, 20];
+
+%% Initialize paths
+
+init_paths
+
+%% Load experiment runs
+
+load_matfiles
 
 %% Plot settings
 
@@ -52,50 +71,43 @@ alpha_lvl = 0.75;
 % Calculate subplot dimensions
 [subplotX, subplotY] = get_subplot_dimensions(num.subjs);
 
-%% Hard coded variables
-
-
-%% Load experiment runs
-
-load_matfiles
-
 %% Analyze behavioral data
 
-init_behav_perf
+analyzeData
 
 %% Stats
 
-test_performance
+% test_performance
 
 %% plot info
 
 
 %% Subject Performance
 
-if plot_ind
+% if plot_ind
 
-    fg  = figure('Color',figure_color);
-    set(0, 'CurrentFigure', fg)
+%     fg  = figure('Color',figure_color);
+%     set(0, 'CurrentFigure', fg)
 
-    for subj = 1:num.subjs
+%     for subj = 1:num.subjs
 
-        plot_performance_ind
+%         plot_performance_ind
 
-    end
+%     end
 
-    close(fg)
+%     close(fg)
 
-end
+% end
 
 %% Group Performance
 
-if plot_grp
+% if plot_grp
 
-    fg  = figure('Color',figure_color);
-    set(0, 'CurrentFigure', fg)
+%     fg  = figure('Color',figure_color);
+%     set(0, 'CurrentFigure', fg)
 
-    plot_performance_grp
+%     plot_performance_grp
 
-    close(fg)
+%     close(fg)
 
-end
+% end
