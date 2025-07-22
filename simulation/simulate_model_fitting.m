7eb0287ad363033c22f20111e1e1558b9cd89675
luisdramirez <luis.ramirez165@gmail.com>
2025-07-22 14:24:17 -0700

    Update simulate_model_fitting.m

--- simulation/simulate_model_fitting.m
+++ simulation/simulate_model_fitting.m
@@ -57,20 +57,26 @@ responses = rand() < p_CW;
 
 %% Grid search for mu and sigma
 
-[best_params, best_sse] = grid_search(rand_offsets, p_CW, guess_rate);
+unique_probe_offsets = sort([-linspace(3, 15, 7), linspace(3, 15, 7)]);
+p_CW = [ 0         0         0    0.5000    0.4000    0.5455    0.2500    0.8333    0.7143    0.6667    1.0000    1.0000    0.5714    1.0000];
+
+% [best_params, best_sse] = grid_search(rand_offsets, p_CW, guess_rate);
+[best_params, best_sse] = grid_search(unique_probe_offsets, p_CW, guess_rate);
+
 
 %% Estimate mu and sigma
 
 options = optimoptions('fmincon');
 
 init_params = best_params;
-init_params = [0,1];
+% init_params = [0,1];
 
 lower_bounds = [-15, 1]; % lower bounds for [mu, sigma]
-upper_bounds = [15, 7];
+upper_bounds = [15, 20];
 
 free_params = init_params;
-fixed_params{1} = [rand_offsets', p_CW'];
+% fixed_params{1} = [rand_offsets', p_CW'];
+fixed_params{1} = [unique_probe_offsets', p_CW'];
 fixed_params{2} = guess_rate;
 
 response_model = @(free_params) calc_fit(free_params, fixed_params);
@@ -85,18 +91,22 @@ sigma = params_est(2);
 x = -20:0.01:20;
 cdf_response = (1 - guess_rate) * normcdf(x, mu, sigma) + 0.5 * guess_rate;
 
+x_exp = (1 - guess_rate) * normcdf(unique_probe_offsets, mu, sigma) + 0.5 * guess_rate;
+r2 = 1 - (sum((p_CW - x_exp).^2) / sum((p_CW - mean(p_CW)).^2));
+
 pdf_response = (1 - guess_rate) * normpdf(x, mu, sigma);
 pdf_response = pdf_response / max(pdf_response); % normalize to peak at 1
 
-sorted_p_CW = sortrows([rand_offsets', p_CW']);
+sorted_p_CW = sortrows([unique_probe_offsets', p_CW']);
 
-figure('Color', 'w');
+figure('Color', 'w','Name', '3 -> 1 contrast');
 plot(x, cdf_response', 'LineWidth', 2,'Color', 'b');
 hold on;
 plot(x, pdf_response', 'LineWidth', 2,'LineStyle', '--','Color', 'b');
+scatter(unique_probe_offsets, p_CW, 30, 'filled');
 line([0,0], [0,1], 'LineWidth', 2, 'Color', 'k');
 line([-20,20], [0.5,0.5], 'LineWidth', 2, 'Color', 'k');
-title(['\mu = ' num2str(round(mu, 2)) ', \sigma = ' num2str(round(sigma, 2))]);
+title(['\mu = ' num2str(round(mu, 2)) ', \sigma = ' num2str(round(sigma, 2)) ' (R^2 = ' num2str(round(r2, 2)) ')']);
 xlabel('Probe Offset');
 ylabel('P(Resp|CW)');
 ylim([0, 1]);
